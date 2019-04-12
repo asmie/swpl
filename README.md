d236be87e7ef58dc7d0a4d16a2bee5655f7e8c77
Piotr Olszewski <asmie@asmie.pl>
2019-04-12 11:03:08 +0200

    Initial files for buildsystem.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # swpl
 SWitchboard PipeLines 
+
+1. General Description
+2. Compilation
+3. Usage
+4. Example data
+
+
+1. General Description
+
+
+
+2. Compilation
+
+
+
+3. Usage
+
+
+
+4. Example data
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+  /**
+ *  @file   main.cpp
+ *  @brief  File with entry point.
+ *
+ *  @author Piotr "asmie" Olszewski
+ *
+ *  @date   2019.04.12
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+
