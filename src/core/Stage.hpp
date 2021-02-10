4204cb97082e11bd7051630b2917b68e516373ba
Piotr Olszewski <asmie@asmie.pl>
2021-02-10 15:31:28 +0100

    Stage file added.

---
+++ src/core/Stage.hpp
@@ -0,0 +1,26 @@
+/**
+ *  @file   Stage.hpp
+ *  @brief  Stage abstraction used to represent the single step of the pipeline.
+ *
+ *  @author Piotr "asmie" Olszewski
+ *
+ *  @date   2020.11.12
+ */
+
+#ifndef SRC_CORE_STAGE_HPP_
+#define SRC_CORE_STAGE_HPP_
+
+class Stage
+{
+public:
+	Stage();
+	virtual ~Stage();
+
+
+private:
+
+};
+
+
+
+#endif /* SRC_CORE_STAGE_HPP_ */
